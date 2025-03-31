9a51bd35a5df311e6a45ea14806ed6b31d30c706
scottroot <allanscotthendrix@gmail.com>
2025-03-31 14:48:43 -0400

    fixed browser crumbs

--- app/BreadcrumbHeader.tsx
+++ app/BreadcrumbHeader.tsx
@@ -13,12 +13,6 @@ export default function BreadcrumbHeader({rootCategory, parentCategory, baseCate
   return (
     <div className="flex flex-col w-full">
       <div>
-        {/*<nav aria-label="Back" className="xsm:hidden">*/}
-        {/*  <Link href="#" className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700">*/}
-        {/*    <ChevronLeftIcon aria-hidden="true" className="-ml-1 mr-1 size-5 shrink-0 text-gray-400" />*/}
-        {/*    Back*/}
-        {/*  </Link>*/}
-        {/*</nav>*/}
         <nav aria-label="Breadcrumb" className="flex xhidden xsm:flex items-center">
           <Link href="/">
             <HomeIcon className="-ml-1 mr-4 size-5 shrink-0 text-gray-400" />
@@ -27,7 +21,7 @@ export default function BreadcrumbHeader({rootCategory, parentCategory, baseCate
             <li>
               <div className="flex">
                 <Link
-                  href={`/${encodeURIComponent(rootCategory)}`}
+                  href={`/browse/${encodeURIComponent(rootCategory)}`}
                   className="text-xs font-medium text-gray-500 hover:text-gray-700"
                 >
                   {rootCategory}
@@ -39,7 +33,7 @@ export default function BreadcrumbHeader({rootCategory, parentCategory, baseCate
                 <div className="flex items-center">
                   <ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-gray-400" />
                   <Link
-                    href={`/${encodeURIComponent(rootCategory)}/${encodeURIComponent(parentCategory)}`}
+                    href={`/browse/${encodeURIComponent(rootCategory)}/${encodeURIComponent(parentCategory)}`}
                     className="ml-4 text-xs font-medium text-gray-500 hover:text-gray-700"
                   >
                     {parentCategory}
@@ -52,7 +46,7 @@ export default function BreadcrumbHeader({rootCategory, parentCategory, baseCate
                 <div className="flex items-center">
                   <ChevronRightIcon aria-hidden="true" className="size-5 shrink-0 text-gray-400" />
                   <Link
-                    href={`/${encodeURIComponent(rootCategory)}/${encodeURIComponent(parentCategory)}/${encodeURIComponent(baseCategory)}`}
+                    href={`/browse/${encodeURIComponent(rootCategory)}/${encodeURIComponent(parentCategory)}/${encodeURIComponent(baseCategory)}`}
                     className="ml-4 text-xs font-medium text-gray-500 hover:text-gray-700"
                   >
                     {baseCategory}
@@ -69,20 +63,6 @@ export default function BreadcrumbHeader({rootCategory, parentCategory, baseCate
             {baseCategory ? baseCategory : (parentCategory ? parentCategory : rootCategory)}
           </h2>
         </div>
-        {/*<div className="mt-4 flex shrink-0 md:ml-4 md:mt-0">*/}
-        {/*  <button*/}
-        {/*    type="button"*/}
-        {/*    className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"*/}
-        {/*  >*/}
-        {/*    Edit*/}
-        {/*  </button>*/}
-        {/*  <button*/}
-        {/*    type="button"*/}
-        {/*    className="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"*/}
-        {/*  >*/}
-        {/*    Publish*/}
-        {/*  </button>*/}
-        {/*</div>*/}
       </div>
     </div>
   )
